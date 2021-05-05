a5b5c2980990cd3d582014ae9c652250aa01e399
Lorenzo <61149733+TinacciL@users.noreply.github.com>
2021-05-05 11:31:38 +0200

    Update isomorphism_molecules.py

--- isomorphism_molecules.py
+++ isomorphism_molecules.py
@@ -1,6 +1,7 @@
 import networkx as nx
 from ase import io, neighborlist, atoms
 
+#function that encode the molecules from xyz file to graph object
 def FromXYZtoGraph(input_file):
     atoms = ['H','He','Li','C','N','O','F','Na','Si','P','S','Cl']
     atomic_numb = [1,2,3,6,7,8,9,11,14,15,16,17]
@@ -31,8 +32,8 @@ mol_1 = FromXYZtoGraph(pathToMolecules/mol_1.xyz)
 #test the isomorphism
 prop = 'atom'
 nm = nx.algorithms.isomorphism.categorical_node_match(prop,prop)
-for index, rows in df_calc_ions.iterrows():
-    if nx.is_isomorphic(FromXYZtoGraph(mol0),FromXYZtoGraph(mol_1),node_match=nm):
-        print('Are isomorphic!')
-    else:
-        print('Are NOT isomorphic!')
+
+if nx.is_isomorphic(mol0,mol_1,node_match=nm):
+    print('Are isomorphic!')
+else:
+    print('Are NOT isomorphic!')
